202580f503e5aa4491abb5272bbc554c299b0f07
miftahunajat <miftahunajat@gmail.com>
2020-01-11 17:52:14 +0700

    chage benchmark ratio

--- src/main/java/com/Config.java
+++ src/main/java/com/Config.java
@@ -3,12 +3,12 @@ package com;
 public class Config {
 //    public static final String INET_ADDRESS_NAME = "10.252.175.161";
     public static final String INET_ADDRESS_NAME = "localhost";
-    public static final int MAX_MATRIX_COUNT = 50_000;
+    public static final int MAX_MATRIX_COUNT = 40_000;
     public static final int MAX_MATRIX_COMBINATION = 499_500;
     public static final int MAX_LIMIT = 1000;
     public static final int PORT = 4031;
     public static final int MAX_CLIENT = 8;
-    public static final int DIVIDER = 4;
+    public static final int DIVIDER = 5;
     public static long checkDuration = 5000;
     public static final String fileLocation = "src/main/resources/1kbigdata.csv";
 
