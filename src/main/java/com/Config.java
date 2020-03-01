f0fd7e0f9282c362b790af430e99594151152816
miftahunajat <miftahunajat@gmail.com>
2020-03-01 22:00:34 +0700

    modify config

--- src/main/java/com/Config.java
+++ src/main/java/com/Config.java
@@ -1,7 +1,7 @@
 package com;
 
 public class Config {
-    public static final String INET_ADDRESS_NAME = "10.252.175.161";
+    public static final String INET_ADDRESS_NAME = "10.252.175.164";
 //    public static final String INET_ADDRESS_NAME = "localhost";
     public static final int MAX_MATRIX_COUNT = 40_000;
     public static final int MAX_MATRIX_COMBINATION = 499_500;
@@ -10,6 +10,6 @@ public class Config {
     public static final int MAX_CLIENT = 8;
     public static final int DIVIDER = 5;
     public static long checkDuration = 5000;
-    public static final String fileLocation = "src/main/resources/ruspini.csv";
+    public static final String fileLocation = "src/main/resources/1kbigdata.csv";
     public static final int INTERVAL = 15;
 }
